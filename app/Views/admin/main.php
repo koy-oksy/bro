369e5d86eb8403dfadda6badb824c6c8381fc37a
artem.klochko <artem.klochko@teapplix.com>
2023-10-12 18:01:41 +0300

    admin panel updated

--- app/Views/admin/main.php
+++ app/Views/admin/main.php
@@ -1,255 +1,69 @@
-<div class="x_panel">
-    <div class="x_title">
-        <h2><i class="fa fa-align-left"></i> Слайдер</h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Settings 1</a>
-                    <a class="dropdown-item" href="#">Settings 2</a>
-                </div>
-            </li>
-            <li><a class="close-link"><i class="fa fa-close"></i></a>
-            </li>
-        </ul>
-        <div class="clearfix"></div>
-    </div>
-    <div class="x_content">
-
-        <!-- start accordion -->
-        <div class="accordion" id="accordion" role="tablist" aria-multiselectable="true">
-            <div class="panel">
-                <a class="panel-heading collapsed" role="tab" id="headingOne" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                    <h4 class="panel-title">Collapsible Group Items #1</h4>
-                </a>
-                <div id="collapseOne" class="panel-collapse in collapse" role="tabpanel" aria-labelledby="headingOne" style="">
-                    <div class="panel-body">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>First Name</th>
-                                    <th>Last Name</th>
-                                    <th>Username</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>Larry</td>
-                                    <td>the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-            <div class="panel">
-                <a class="panel-heading collapsed" role="tab" id="headingTwo" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    <h4 class="panel-title">Collapsible Group Items #2</h4>
-                </a>
-                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo" style="">
-                    <div class="panel-body">
-                        <p><strong>Collapsible Item 2 data</strong>
-                        </p>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                    </div>
-                </div>
-            </div>
-            <div class="panel">
-                <a class="panel-heading collapsed" role="tab" id="headingThree" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    <h4 class="panel-title">Collapsible Group Items #3</h4>
-                </a>
-                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree" style="">
-                    <div class="panel-body">
-                        <p><strong>Collapsible Item 3 data</strong>
-                        </p>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor
-                    </div>
-                </div>
-            </div>
+<div class="clearfix"></div>
+<div class="page-title">
+    <div class="row">
+        <div class="col-6">
+            <h3>Шаблон головної сторінки</h3>
         </div>
-        <!-- end of accordion -->
-
-
-    </div>
-</div>
-
-<div class="x_panel">
-    <div class="x_title">
-        <h2><i class="fa fa-question"></i> Про нас</h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Settings 1</a>
-                    <a class="dropdown-item" href="#">Settings 2</a>
-                </div>
-            </li>
-            <li><a class="close-link"><i class="fa fa-close"></i></a>
-            </li>
-        </ul>
-        <div class="clearfix"></div>
-    </div>
-    <div class="x_content">
-
-        <p>Add small overlays of content for housing secondary information.</p>
-        <!-- start pop-over -->
-        <div class="bs-example-popovers">
-            <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="left" title="" data-original-title="Tooltip left">left</button>
-            <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="" data-original-title="Tooltip top">Top</button>
-            <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Tooltip bottom">Bottom</button>
-            <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" title="" data-original-title="Tooltip right">Right</button>
-        </div>
-        <!-- end pop-over -->
-
-    </div>
-</div>
-
-<div class="x_panel">
-    <div class="x_title">
-        <h2><i class="fa fa-bars"></i> Лічильники</h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Settings 1</a>
-                    <a class="dropdown-item" href="#">Settings 2</a>
-                </div>
-            </li>
-            <li><a class="close-link"><i class="fa fa-close"></i></a>
-            </li>
-        </ul>
-        <div class="clearfix"></div>
-    </div>
-    <div class="x_content">
-
-        <ul class="nav nav-tabs bar_tabs" id="myTab" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
-            </li>
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade active show" id="home" role="tabpanel" aria-labelledby="home-tab">
-                Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher
-                synth. Cosby sweater eu banh mi, qui irure terr.
-            </div>
-            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo
-                booth letterpress, commodo enim craft beer mlkshk aliquip
-            </div>
-            <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                xxFood truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo
-                booth letterpress, commodo enim craft beer mlkshk 
-            </div>
+        <div class="col-6" style="padding: 10px">
+            Шрифт:
+            <select id="selectFont">
+                <option>Roboto Mono</option>
+                <option>Consolas</option>
+                <option>Cascadia Mono</option>
+                <option>Inconsolata</option>
+                <option>Source Code Pro</option>
+                <option>IBM Plex Mono</option>
+                <option>Space Mono</option>
+                <option>PT Mono</option>
+                <option>Ubuntu Mono</option>
+                <option>Nanum Gothic Coding</option>
+                <option>Cousine</option>
+                <option>Fira Mono</option>
+                <option>Share Tech Mono</option>
+                <option>Courier Prime</option>
+                <option>Anonymous Pro</option>
+                <option>Cutive Mono</option>
+                <option>VT323</option>
+                <option>JetBrains Mono</option>
+                <option>Noto Sans Mono</option>
+                <option>Red Hat Mono</option>
+                <option>Martian Mono</option>
+                <option>Major Mono Display</option>
+                <option>Nova Mono</option>
+                <option>Syne Mono</option>
+                <option>Xanh Mono</option>
+                <option>Monofett</option>
+            </select>
+            Розмір шрифту (пікс):
+            <input id="inputFontSize" type="number" step=".1" value="12" style="width: 50px;" />
         </div>
     </div>
 </div>
 
-<div class="x_panel">
-    <div class="x_title">
-        <h2><i class="fa fa-align-left"></i> Чому варто піти з нами?</h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Settings 1</a>
-                    <a class="dropdown-item" href="#">Settings 2</a>
-                </div>
-            </li>
-            <li><a class="close-link"><i class="fa fa-close"></i></a>
-            </li>
-        </ul>
-        <div class="clearfix"></div>
-    </div>
-    <div class="x_content">
+<div class="clearfix"></div>
 
-        <!-- start accordion -->
-        <div class="accordion" id="accordion" role="tablist" aria-multiselectable="true">
-            <div class="panel">
-                <a class="panel-heading collapsed" role="tab" id="headingTwo" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    <h4 class="panel-title">Collapsible Group Items #2</h4>
-                </a>
-                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo" style="">
-                    <div class="panel-body">
-                        <p><strong>Collapsible Item 2 data</strong>
-                        </p>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
-                    </div>
+<div class="row">
+    <div class="x_panel">
+        <div class="x_content">
+            {!form_open!}
+
+            <input type="hidden" value="{template_content}" id="html"/>
+            <input id="selectStyle" type="hidden" value="vs2015.min.css"/>
+            <input id="selectLanguage" type="hidden" value="language-html"/>
+            
+            <div id="divCodeWrapper">
+                <pre id="preCode"><code id="codeBlock" class="language-html"></code></pre>
+                <textarea id="textarea1" wrap="soft" spellcheck="false" name="template_code"></textarea>
+            </div>
+            
+            <div class="ln_solid"></div>
+            <div class="item form-group">
+                <div class="col-md-6 col-sm-6 offset-md-4">
+                    <button class="btn btn-primary reset_template" template_name="{template_name}" type="button"><i class="fa fa-close"></i> Скинути зміни</button>
+                    <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Зберігти зміни</button>
                 </div>
             </div>
-            <div class="panel">
-                <a class="panel-heading collapsed" role="tab" id="headingThree" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    <h4 class="panel-title">Collapsible Group Items #3</h4>
-                </a>
-                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree" style="">
-                    <div class="panel-body">
-                        <p><strong>Collapsible Item 3 data</strong>
-                        </p>
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor
-                    </div>
-                </div>
-            </div>
+            {!form_close!}
         </div>
-        <!-- end of accordion -->
-
-
-    </div>
-</div>
-
-<div class="x_panel">
-    <div class="x_title">
-        <h2><i class="fa fa-image"></i> Фото з посиланням</h2>
-        <ul class="nav navbar-right panel_toolbox">
-            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Settings 1</a>
-                    <a class="dropdown-item" href="#">Settings 2</a>
-                </div>
-            </li>
-            <li><a class="close-link"><i class="fa fa-close"></i></a>
-            </li>
-        </ul>
-        <div class="clearfix"></div>
-    </div>
-    <div class="x_content">
-
-        <div class="bs-example" data-example-id="simple-jumbotron">
-            <div class="jumbotron">
-                <h1>Hello, world!</h1>
-                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            </div>
-        </div>
-
     </div>
 </div>
