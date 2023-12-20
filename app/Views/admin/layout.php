59af20f45b2db905a561b582a2970a5e8a01ee32
Ksu <85317684+koy-oksy@users.noreply.github.com>
2023-12-20 22:47:32 +0200

    admin

--- app/Views/admin/layout.php
+++ app/Views/admin/layout.php
@@ -42,10 +42,7 @@
         <div class="main_container">
             <div class="col-md-3 left_col">
                 <div class="left_col scroll-view">
-                    <div class="navbar nav_title" style="border: 0;">
-                     
-                    </div>
-
+                  
                     <div class="clearfix"></div>
 
                     <!-- menu profile quick info -->
